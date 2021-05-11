02866a6c157ae52ff0d5addbc5b28523c61da783
littleOneYuan <53067129+littleOneYuan@users.noreply.github.com>
2021-05-11 15:46:24 +0800

    Update index.vue

--- a-time/index.vue
+++ a-time/index.vue
@@ -1,65 +1,61 @@
 <template>
-  <div class="a-check">
+  <div class="attr-check">
     <Row>
       <Col flex="110px">
         <span class="c-fix-title wt">{{ attr_name }}</span>
       </Col>
-      <!-- 时间 -->
-      <Col flex="auto">
-        <date-pick
-          type="daterange"
-          :value="datetime"
-          :options="datetimeOption"
-          confirm
-          :clearable="false"
-          :editable="false"
-          split-panels
-          :transfer="false"
-          @on-change="handleDatetime"
-          @on-ok="confirmDatetime"
-          @on-clear="day_clear"
-          @on-clear-shortcut="clearShortCut"
-          placement="bottom-start"
-          placeholder="请选择查询日期范围"
-          style="width: 200px"
-        />
+      <Col flex="90px" v-if="nolimit">
+        <Button v-if="checked" type="primary" @click="noLimit">不限</Button>
+        <Button v-else @click="noLimit">不限</Button>
+      </Col>
+      <!-- 充值情况 -->
+      <Col flex="auto" v-if="atype === 'pay'">
+        <Button type="primary" ghost @click="select_show = !select_show">{{
+          btn_name[0]
+        }}</Button>
+        <Select v-model="dim" style="width: 140px" @on-open-change="dim_change">
+          <Option v-for="item in attrBox.dimList" :value="item.value" :key="item.value">{{
+            item.label
+          }}</Option>
+        </Select>
+        <cCompare />
       </Col>
     </Row>
     <Divider />
   </div>
 </template>
 <script>
-import shortcuts from '@/data/shortcuts'
-import DatePick from '@/components/date-picker'
-// import { formatDate } from '@/common/js/utils'
+import cCompare from '@/components/c-compare'
+import attrBox from '@/data/user/attr_box'
 export default {
-  name: 'a-check',
+  name: 'attr-check',
   components: {
-    DatePick
+    cCompare
   },
   data () {
-    // const date = new Date()
-    // var fday
-    // if (date.getMonth() < 9) {
-    //   fday = date.getFullYear() + '-0' + (date.getMonth() + 1) + '-01'
-    // } else {
-    //   fday = date.getFullYear() + '-' + (date.getMonth() + 1) + '-01'
-    // }
-    // const now = formatDate(date, 'yyyy-MM-dd') // 初始化默认时间
     return {
-      datetime: ['', ''],
-      // datetime: [fday, now],
-      comDate: ['', ''],
-      // comDate: [fday, now],
-      datetimeOption: {
-        shortcuts,
-        disabledDate (date) {
-          return date && date.valueOf() > Date.now()
-        }
-      }
+      attr_List: deepCopy(this.attrList),
+      res_attr_List: [],
+      sel_attr_List: [],
+      checked: true,
+      hasPhone: false,
+      attrBox: attrBox,
+      dim: 1
     }
   },
   props: {
+    atype: {
+      type: String,
+      default () {
+        return 'check'
+      }
+    },
+    nolimit: {
+      type: Boolean,
+      default () {
+        return true
+      }
+    },
     attr_name: {
       type: String,
       default () {
@@ -67,44 +63,34 @@ export default {
       }
     }
   },
-  computed: {},
   methods: {
-    // 日期选择器
-    handleDatetime (val) {
-      this.datetime = val
-    },
-    confirmDatetime () {
-      this.comDate = JSON.parse(JSON.stringify(this.datetime))
-      this.$emit('getData', this.comDate, this.attr_name)
-    },
-    day_clear () {
-      // const date = new Date()
-      // var fday
-      // if (date.getMonth() < 9) {
-      //   fday = date.getFullYear() + '-0' + (date.getMonth() + 1) + '-01'
-      // } else {
-      //   fday = date.getFullYear() + '-' + (date.getMonth() + 1) + '-01'
-      // }
-      // const now = formatDate(date, 'yyyy-MM-dd') // 初始化默认时间
-      // this.datetime = [fday, now]
-      this.datetime = ['', '']
-      this.comDate = JSON.parse(JSON.stringify(this.datetime))
-      this.$emit('getData', this.comDate, this.attr_name)
-    },
-    clearShortCut () {
-      const shortCut = document.querySelectorAll('.ivu-picker-panel-shortcut')
-      const len = shortCut.length
-      for (let i = 0; i < len; i++) {
-        shortCut[i].style.background = 'none'
+    // 限制还是不限
+    noLimit () {
+      this.checked = !this.checked
+      if (this.checked) {
+        // 选中不限
+        this.$Message.info('选择不限，默认选项也就失效了哦o(*￣▽￣*)ブ')
+      } else {
+        this.$Message.info('不做任何选择，默认不限哦o(*￣▽￣*)ブ')
       }
+    },
+    dim_change () {
+      console.log(this.dim, '---dim')
     }
   },
   watch: {},
-  created () {}
+  created () {
+  }
 }
 </script>
 <style lang="stylus" scoped>
-// /deep/.ivu-picker-confirm>.ivu-btn:first-child {
-//   display: none;
-// }
+.ivu-row {
+  margin-top: 8px;
+}
+.ivu-btn {
+  margin-right: 10px
+}
+.ivu-select {
+  margin-right: 10px
+}
 </style>
